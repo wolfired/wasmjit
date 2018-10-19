8794c93ef73f9a101c349c661b43099b0d396078
Rian Hunter <rian@alum.mit.edu>
2018-10-18 20:17:57 -0700

    Add FD_* macros for kernel implementation of select()

--- src/wasmjit/posix_sys.h
+++ src/wasmjit/posix_sys.h
@@ -40,6 +40,11 @@ typedef unsigned int nfds_t;
 
 #define SYS_CMSG_NXTHDR(msg, cmsg) __CMSG_NXTHDR((msg)->msg_control, (msg)->msg_controllen, (cmsg))
 
+#define FD_ZERO(s) do { int __i; unsigned long *__b=(s)->fds_bits; for(__i=sizeof (fd_set)/sizeof (long); __i; __i--) *__b++=0; } while(0)
+#define FD_SET(d, s)   ((s)->fds_bits[(d)/(8*sizeof(long))] |= (1UL<<((d)%(8*sizeof(long)))))
+#define FD_CLR(d, s)   ((s)->fds_bits[(d)/(8*sizeof(long))] &= ~(1UL<<((d)%(8*sizeof(long)))))
+#define FD_ISSET(d, s) !!((s)->fds_bits[(d)/(8*sizeof(long))] & (1UL<<((d)%(8*sizeof(long)))))
+
 #else
 
 #include <errno.h>
