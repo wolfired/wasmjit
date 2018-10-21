53a7f996cdc7fa8b5b42ec934e302828005ddd78
Rian Hunter <rian@alum.mit.edu>
2018-10-21 13:55:08 -0700

    'char' isn't guaranteed to be signed

--- src/wasmjit/compile.c
+++ src/wasmjit/compile.c
@@ -132,7 +132,7 @@ static void encode_le_uint32_t(uint32_t val, char *buf)
 
 #define OUTB(b)						   \
 	do {						   \
-		char __b;				   \
+		signed char __b;				\
 		assert((b) <= 127 && ((intmax_t)(b)) >= -128);  \
 		__b = (b);				   \
 		if (!output_buf(output, &__b, 1))	   \
